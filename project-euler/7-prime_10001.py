025922b1af834850396415c48c2b989fb984f1cf
Luis Miranda <luyimiranda07@gmail.com>
2020-06-07 22:16:36 -0500

    Improves for challenge 7 PE

--- project-euler/7-prime_10001.py
+++ project-euler/7-prime_10001.py
@@ -1,11 +1,11 @@
 def gen_primes(max_num):
-    primes = []
-    n = 2
+    primes = [2]
+    n = 3
     while len(primes) < max_num:
-        if all(n % p > 0 for p in range(2, int(n**.5)+1)):
+        if all(n % p > 0 for p in range(3, int(n**.5)+1)):
             primes.append(n)
 
-        n+=1
+        n+=2
 
     return max(primes)
 
